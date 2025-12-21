bc015eb975194fb5a87635ba9343b11921fb521b
Soeur Timëo <t.soeur@student.helmo.be>
2025-12-21 10:13:04 +0100

    add global log

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { UsersModule } from './users/users.module';
 import { HealthController } from './health/health.controller';
 import { AuthModule } from './auth/auth.module';
 import { FamilyModule } from './family/family.module';
+import { CLoggingModule } from './c-logging/c-logging.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule, FamilyModule],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+    FamilyModule,
+    CLoggingModule,
+  ],
   controllers: [HealthController],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CLoggingService } from './c-logging/c-logging.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }));
+  app.useLogger(app.get(CLoggingService));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
