2aa1cdd3972c6eed0c574916279a65d67155839b
Timëo <t.soeur@student.helmo.be>
2026-03-09 08:30:01 +0100

    format

--- src/main.ts
+++ src/main.ts
@@ -9,21 +9,23 @@ async function bootstrap() {
   app.use(cookieParser(process.env.COOKIE_SECRET));
 
   app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-        transformOptions: {
-          enableImplicitConversion: true,
-        },
-      }),
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
   );
 
   // Prefix global
   app.setGlobalPrefix('api/v1');
 
   app.enableCors({
-    origin: process.env.ALLOWED_ORIGINS?.split(',') ?? ['http://localhost:3001'],
+    origin: process.env.ALLOWED_ORIGINS?.split(',') ?? [
+      'http://localhost:3001',
+    ],
     credentials: true,
   });
 
