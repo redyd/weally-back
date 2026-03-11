ea5b0010a16f464a657aeff15ba7460670d62795
Timëo <t.soeur@student.helmo.be>
2026-03-11 21:04:53 +0100

    better auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,29 +1,34 @@
-import { betterAuth } from 'better-auth';
-import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { PrismaClient } from '@prisma/client';
+import {betterAuth} from 'better-auth';
+import {prismaAdapter} from 'better-auth/adapters/prisma';
+import {PrismaClient} from '@prisma/client';
+import {expo} from "@better-auth/expo";
 
 const prisma = new PrismaClient();
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL, // ex: http://localhost:3000
-  secret: process.env.BETTER_AUTH_SECRET,
+    baseURL: process.env.BETTER_AUTH_URL, // ex: http://localhost:3000
+    secret: process.env.BETTER_AUTH_SECRET,
 
-  database: prismaAdapter(prisma, {
-    provider: 'postgresql',
-  }),
+    plugins: [
+        expo(),
+    ],
 
-  // Mapper les champs BetterAuth vers ton modèle User existant
-  user: {
-    fields: {
-      name: 'name',
+    database: prismaAdapter(prisma, {
+        provider: 'postgresql',
+    }),
+
+    // Mapper les champs BetterAuth vers ton modèle User existant
+    user: {
+        fields: {
+            name: 'name',
+        },
     },
-  },
 
-  emailAndPassword: {
-    enabled: true,
-  },
+    emailAndPassword: {
+        enabled: true,
+    },
 
-  trustedOrigins: [process.env.FRONTEND_URL ?? 'http://localhost:4200'],
+    trustedOrigins: [process.env.FRONTEND_URL ?? 'http://localhost:4200'],
 });
 
 export type Auth = typeof auth;
